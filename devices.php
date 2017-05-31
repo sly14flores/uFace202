3ff4e0278271109754d0a64519790ba4dbafbdc3
Sly Flores <sly@christian.com.ph>
2017-05-31 17:59:00 +0800

    added new device

--- devices.php
+++ devices.php
@@ -11,7 +11,8 @@ $devices = array(
 		"3574154000116"=>array("File"=>"3574154000116_attlog.dat","No"=>"2","Description"=>"Ground Floor, Information Center"),
 		"3574154000008"=>array("File"=>"3574154000008_attlog.dat","No"=>"3","Description"=>"Second Floor"),
 		"3574154000091"=>array("File"=>"3574154000091_attlog.dat","No"=>"4","Description"=>"Engineering"),
-		"3574154000004"=>array("File"=>"3574154000004_attlog.dat","No"=>"5","Description"=>"Admin")
+		"3574154000004"=>array("File"=>"3574154000004_attlog.dat","No"=>"5","Description"=>"Admin"),
+		"OPH6100066092900002"=>array("File"=>"OPH6100066092900002_attlog.dat","No"=>"6","Description"=>"uface 402 no 1")
 	);
 
 $device_description = array(1=>"Ground Floor, LBP ATM",2=>"Ground Floor, Information Center",3=>"Second Floor",4=>"Engineering",5=>"Admin");
